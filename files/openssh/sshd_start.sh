fff640ac5f9047aba5f2d8042e189562352d0546
Jonathan Hull <43802921+IguanaBob@users.noreply.github.com>
2024-12-06 08:48:09 -0600

    Update sshd_start.sh
    Fix perms on keys after generating.

--- files/openssh/sshd_start.sh
+++ files/openssh/sshd_start.sh
@@ -6,18 +6,21 @@ if [ ! -f "$key_file_path/ssh_host_rsa_key" ]
 then
     echo "ssh_host_rsa_key file no exit, creating..."
     ssh-keygen -t rsa -f $key_file_path/ssh_host_rsa_key -N ""
+    chmod 600 $key_file_path/ssh_host_rsa_key
 fi
 
 if [ ! -f "$key_file_path/ssh_host_ecdsa_key" ]
 then
     echo "ssh_host_ecdsa_key file no exit, creating..."
     ssh-keygen -t rsa -f $key_file_path/ssh_host_ecdsa_key -N ""
+    chmod 600 $key_file_path/ssh_host_ecdsa_key
 fi
 
 if [ ! -f "$key_file_path/ssh_host_ed25519_key" ]
 then
     echo "ssh_host_ed25519_key file no exit, creating..."
     ssh-keygen -t rsa -f $key_file_path/ssh_host_ed25519_key -N ""
+    chmod 600 $key_file_path/ssh_host_ed25519_key
 fi
 
 is_cfg=$(cat $key_file_path/sshd_config | grep "HostKey /ac_lib/lib/openssh/etc/ssh_host_rsa_key")
