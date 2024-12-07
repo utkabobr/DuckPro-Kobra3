5f84ed4ffca331e14e9b763f2b71eadab4d83168
Julien Batonnet <julien.batonnet@gmail.com>
2024-12-07 11:53:42 -0500

    Fix an underscore mistake. Sorry everyone !

--- files/home/ytka/mjpg-streamer.sh
+++
@@ -1,6 +0,0 @@
-if [ -e /useremain/home/ytka/printer_data/config/.camera ]; then
-    if [ -e /dev/video10 ]; then
-        killall gkcam
-        /useremain/dist/mjpg-streamer/mjpg_streamer -i "input_uvc.so -d /dev/video10 -n -r 1280x720" -o "output_http.so -w /useremain/dist/mjpg-streamer/www"
-    fi
-fi
